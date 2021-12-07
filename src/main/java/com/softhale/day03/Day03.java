08041a091d92b111ea0e31899b3a1825ec3cde65
Boisababy Clips <bob@fleming.nowhere>
2021-12-07 08:50:55 +0100

    Solution to day 3 part 2

--- src/main/java/com/softhale/day03/Day03.java
+++ src/main/java/com/softhale/day03/Day03.java
@@ -3,14 +3,18 @@ package com.softhale.day03;
 import com.softhale.utils.ParserUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class Day03 {
 
     private final ParserUtils parserUtils = new ParserUtils();
 
-    public int part1(String filePath) throws IOException {
+    public int part1(String filePath) {
         var lines = parserUtils.readLines(filePath);
         var content = new Character[lines.size()][];
 
@@ -52,7 +56,102 @@ public class Day03 {
         return gamma * epsilon;
     }
 
-    public int part2(String filePath) throws IOException {
-        return 0;
+    public int part2(String filePath) {
+        var lines = parserUtils.readLines(filePath);
+
+        var cells = toCells(
+                lines,
+                s -> s.chars().mapToObj(c -> (char) c).toList());
+
+        int oxygenGeneratorRating = calculateOxygenGeneratorRating(cells, lines, 0);
+        int co2ScrubberRating = calculateCO2ScrubberRating(cells, lines, 0);
+
+        return oxygenGeneratorRating * co2ScrubberRating;
+    }
+
+    private int calculateOxygenGeneratorRating(List<Cell<Character>> cells, List<String> lines, int bitIndex) {
+        if (lines.size() == 1) {
+            return Integer.parseInt(lines.get(0), 2);
+        }
+
+        var transposed = extractColumnValues(cells, bitIndex);
+        var frequency = countFrequency(transposed);
+        var countOfOne = frequency.get('1');
+        var countOfZero = frequency.get('0');
+        var charToMatch = countOfOne >= countOfZero ? '1' : '0';
+
+        var filteredLines = lines.stream()
+                .filter(line -> line.charAt(bitIndex) == charToMatch)
+                .toList();
+
+        var filteredCells = toCells(
+                filteredLines,
+                s -> s.chars().mapToObj(c -> (char) c).toList());
+
+        return calculateOxygenGeneratorRating(filteredCells, filteredLines, bitIndex + 1);
+    }
+
+    private int calculateCO2ScrubberRating(List<Cell<Character>> cells, List<String> lines, int bitIndex) {
+        if (lines.size() == 1) {
+            return Integer.parseInt(lines.get(0), 2);
+        }
+
+        var transposed = extractColumnValues(cells, bitIndex);
+        var frequency = countFrequency(transposed);
+        var countOfOne = frequency.get('1');
+        var countOfZero = frequency.get('0');
+        var charToMatch = countOfZero <= countOfOne ? '0' : '1';
+
+        var filteredLines = lines.stream()
+                .filter(line -> line.charAt(bitIndex) == charToMatch)
+                .toList();
+
+        var filteredCells = toCells(
+                filteredLines,
+                s -> s.chars().mapToObj(c -> (char) c).toList());
+
+        return calculateCO2ScrubberRating(filteredCells, filteredLines, bitIndex + 1);
+    }
+
+    private Map<Character, Integer> countFrequency(String str) {
+        var map = new HashMap<Character, Integer>();
+
+        str.chars().forEach(c -> {
+            var character = Character.valueOf((char) c);
+            var counter = map.getOrDefault(character, 0);
+            map.put((char) c, ++counter);
+        });
+
+        return map;
+    }
+
+    private String extractColumnValues(List<Cell<Character>> cells, int columnIndex) {
+        var stringBuilder = new StringBuilder();
+
+        cells.stream()
+                .filter(c -> c.location().x == columnIndex)
+                .forEach(c -> stringBuilder.append(c.content));
+
+        return stringBuilder.toString();
+    }
+
+    private <T> List<Cell<T>> toCells(List<String> lines,
+                                      Function<String, List<T>> lineToTypeConverterFn) {
+        var cells = new ArrayList<Cell<T>>();
+
+        for (int y = 0; y < lines.size(); y++) {
+            var ts = lineToTypeConverterFn.apply(lines.get(y));
+
+            for (int x = 0; x < ts.size(); x++) {
+                var cell = new Cell<T>(new Point(x, y), ts.get(x));
+                cells.add(cell);
+            }
+        }
+
+        return cells;
+    }
+
+
+    public static record Cell<T>(Point location, T content) {
     }
 }
--- src/test/java/com/softhale/day03/Day03Test.java
+++ src/test/java/com/softhale/day03/Day03Test.java
@@ -2,8 +2,6 @@ package com.softhale.day03;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Day03Test {
@@ -11,26 +9,26 @@ public class Day03Test {
     private final Day03 day03 = new Day03();
 
     @Test
-    void part1_whenTestData_shouldReturn198() throws IOException {
+    void part1_whenTestData_shouldReturn198() {
         var result = day03.part1("src/test/resources/day03.txt");
         assertThat(result).isEqualTo(198);
     }
 
     @Test
-    void part1_whenRealData_shouldReturn() throws IOException {
+    void part1_whenRealData_shouldReturnCorrectValue() {
         var result = day03.part1("src/main/resources/day03.txt");
         assertThat(result).isEqualTo(2648450);
     }
 
     @Test
-    void part2_whenTestData_shouldReturn() throws IOException {
+    void part2_whenTestData_shouldReturn230() {
         var result = day03.part2("src/test/resources/day03.txt");
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(230);
     }
 
     @Test
-    void part2_whenRealData_shouldReturn() throws IOException {
+    void part2_whenRealData_shouldReturnCorrectValue() {
         var result = day03.part2("src/main/resources/day03.txt");
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(2845944);
     }
 }
