5faebe4492c3bec21b852426f7ad1281d02f8f26
Boisababy Clips <bob@fleming.nowhere>
2021-12-21 23:33:51 +0100

    Setup for day 12

---
+++ src/test/java/com/softhale/day12/Day12Test.java
@@ -0,0 +1,34 @@
+package com.softhale.day12;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day12Test {
+
+    private final Day12 day12 = new Day12();
+
+    @Test
+    void part1_whenTestData_shouldReturnCorrectValue() {
+        var result = day12.part1("src/test/resources/day12.txt");
+        assertThat(result).isEqualTo(226L);
+    }
+
+    @Test
+    void part1_whenRealData_shouldReturnCorrectValue() {
+        var result = day12.part1("src/main/resources/day12.txt");
+        assertThat(result).isEqualTo(-1L);
+    }
+
+    @Test
+    void part2_whenTestData_shouldReturnCorrectValue() {
+        var result = day12.part2("src/test/resources/day12.txt");
+        assertThat(result).isEqualTo(-1L);
+    }
+
+    @Test
+    void part2_whenRealData_shouldReturnCorrectValue() {
+        var result = day12.part2("src/main/resources/day12.txt");
+        assertThat(result).isEqualTo(-1L);
+    }
+}
