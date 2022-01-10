458d7b0ee7655326e0e8271b246deca247305ef5
Pawel Tylczak <p-tylczak@gmail.com>
2022-01-10 23:52:48 +0100

    Solution to day 15

--- src/main/java/com/softhale/day15/Day15.java
+++ src/main/java/com/softhale/day15/Day15.java
@@ -6,10 +6,9 @@ import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
-import java.util.Arrays;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -23,8 +22,77 @@ public class Day15 {
     public long part1(String filePath, int maxX, int maxY) {
         var cells = boardUtils.toCells(
                 parserUtils.readLines(filePath),
-                line -> Arrays.stream(line.split("")).map(Integer::valueOf).toList());
+                line -> Arrays.stream(line.split(""))
+                        .map(Integer::valueOf)
+                        .toList());
 
+        return findShortestPathWeight(maxX, maxY, cells);
+    }
+
+    public long part2(String filePath, int maxX, int maxY) {
+        var cells = boardUtils.toCells(
+                parserUtils.readLines(filePath),
+                line -> Arrays.stream(line.split(""))
+                        .map(Integer::valueOf)
+                        .toList());
+
+        var cellByLocation = cells.stream()
+                .collect(Collectors.toMap(this::toKey, Function.identity()));
+
+        var extendedCells = new ArrayList<>(cells);
+
+        for (int index = 1; index < 15; index++) {
+            for (int y = 0; y < maxY; y++) {
+                for (int x = 0; x < maxX; x++) {
+                    var cell = cellByLocation.get(toKey(x, y));
+                    var newContent = cell.content() + index;
+                    if (newContent > 9) {
+                        newContent = newContent - 9;
+                    }
+
+                    var newX = cell.location().x + (index * maxX);
+
+                    var newCell = new BoardUtils.Cell<>(
+                            new Point(newX, cell.location().y),
+                            newContent);
+
+                    extendedCells.add(newCell);
+                }
+            }
+        }
+
+        cellByLocation = extendedCells.stream()
+                .collect(Collectors.toMap(this::toKey, Function.identity()));
+
+        var newX = 0;
+        var newY = 0;
+        var newCells = new ArrayList<BoardUtils.Cell<Integer>>();
+
+        for (int index = 0; index < 5; index++) {
+            for (int y = 0; y < maxY; y++) {
+                for (int x = index * maxX; x < maxX * 5 + (index * maxX); x++) {
+                    // if (x % maxX == 0)
+                    //    System.out.print(" ");
+
+                    var cell = cellByLocation.get(toKey(x, y));
+                    // System.out.print(cell.content());
+                    newCells.add(new BoardUtils.Cell<>(
+                            new Point(newX, newY),
+                            cell.content()));
+
+                    newX++;
+                }
+
+                newX = 0;
+                newY++;
+                System.out.println();
+            }
+        }
+
+        return findShortestPathWeight(maxX * 5, maxY * 5, newCells);
+    }
+
+    private long findShortestPathWeight(int maxX, int maxY, List<BoardUtils.Cell<Integer>> cells) {
         var graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
 
         var cellByLocation = cells.stream()
@@ -34,7 +102,7 @@ public class Day15 {
         for (int y = 0; y < maxY; y++) {
             for (int x = 0; x < maxX; x++) {
                 var adjacent = findAdjacentCells(x, y, cellByLocation);
-                var thisCell = cellByLocation.get(x + "_" + y);
+                var thisCell = cellByLocation.get(toKey(x, y));
                 adjacent.forEach(a -> {
                     var edge = graph.addEdge(thisCell, a);
                     graph.setEdgeWeight(edge, a.content());
@@ -45,18 +113,18 @@ public class Day15 {
         var alg = new DijkstraShortestPath<>(graph);
         var path = alg.getPath(
                 cellByLocation.get("0_0"),
-                cellByLocation.get((maxX - 1) + "_" + (maxY - 1)));
+                cellByLocation.get(toKey((maxX - 1), (maxY - 1))));
 
         return (long) path.getWeight();
     }
 
-    private List<BoardUtils.Cell<Integer>> findAdjacentCells(int x, int y, Map<String, BoardUtils.Cell<Integer>> cells) {
-        BiFunction<Integer, Integer, String> keyFn = (i1, i2) -> i1 + "_" + i2;
+    private List<BoardUtils.Cell<Integer>> findAdjacentCells(int x, int y, Map<
+            String, BoardUtils.Cell<Integer>> cells) {
 
-        var n = cells.get(keyFn.apply(x, y - 1));
-        var s = cells.get(keyFn.apply(x, y + 1));
-        var e = cells.get(keyFn.apply(x + 1, y));
-        var w = cells.get(keyFn.apply(x - 1, y));
+        var n = cells.get(toKey(x, y - 1));
+        var s = cells.get(toKey(x, y + 1));
+        var e = cells.get(toKey(x + 1, y));
+        var w = cells.get(toKey(x - 1, y));
 
         return Stream.of(n, s, e, w)
                 .filter(Objects::nonNull)
@@ -64,10 +132,10 @@ public class Day15 {
     }
 
     private String toKey(BoardUtils.Cell<Integer> cell) {
-        return cell.location().x + "_" + cell.location().y;
+        return toKey(cell.location().x, cell.location().y);
     }
 
-    public long part2(String filePath) {
-        return 0L;
+    private String toKey(int x, int y) {
+        return x + "_" + y;
     }
 }
--- src/test/java/com/softhale/day15/Day15Test.java
+++ src/test/java/com/softhale/day15/Day15Test.java
@@ -17,18 +17,18 @@ class Day15Test {
     @Test
     void part1_whenRealData_shouldReturnCorrectValue() {
         var result = day15.part1("src/main/resources/day15.txt", 100, 100);
-        assertThat(result).isEqualTo(-1L);
+        assertThat(result).isEqualTo(739L);
     }
 
     @Test
     void part2_whenTestData_shouldReturnCorrectValue() {
-        var result = day15.part2("src/test/resources/day15.txt");
-        assertThat(result).isEqualTo(-1L);
+        var result = day15.part2("src/test/resources/day15.txt", 10, 10);
+        assertThat(result).isEqualTo(315L);
     }
 
     @Test
     void part2_whenRealData_shouldReturnCorrectValue() {
-        var result = day15.part2("src/main/resources/day15.txt");
-        assertThat(result).isEqualTo(-1L);
+        var result = day15.part2("src/main/resources/day15.txt", 100, 100);
+        assertThat(result).isEqualTo(3040L);
     }
 }
