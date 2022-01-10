b733b912971f56a59afc1952ecbf47ef4bffcf30
Pawel Tylczak <p-tylczak@gmail.com>
2022-01-11 00:14:29 +0100

    Minor refactor of solution to day 12

--- src/main/java/com/softhale/day12/Day12.java
+++ src/main/java/com/softhale/day12/Day12.java
@@ -9,26 +9,20 @@ public class Day12 {
     private final ParserUtils parserUtils = new ParserUtils();
 
     public long part1(String filePath) {
-        var edges = parserUtils.readLines(filePath).stream()
-                .map(line -> {
-                    var parts = line.split("-");
-                    var from = parts[0];
-                    var to = parts[1];
+        var nodes = createUndirectedGraph(filePath).getNodes();
 
-                    return new DefaultKeyValue<>(from, to);
-                })
-                .toList();
-
-        var graph = new UndirectedGraph<String>();
-        for (var edge : edges) {
-            graph.addEdge(edge.getKey(), edge.getValue());
-        }
-
-        return new GraphPathCounterPart1<>(graph.getNodes())
+        return new GraphPathCounterPart1<>(nodes)
                 .countPaths("start", "end");
     }
 
     public long part2(String filePath) {
+        var nodes = createUndirectedGraph(filePath).getNodes();
+
+        return new GraphPathCounterPart2<>(nodes)
+                .countPaths("start", "end");
+    }
+
+    private UndirectedGraph<String> createUndirectedGraph(String filePath) {
         var edges = parserUtils.readLines(filePath).stream()
                 .map(line -> {
                     var parts = line.split("-");
@@ -44,7 +38,6 @@ public class Day12 {
             graph.addEdge(edge.getKey(), edge.getValue());
         }
 
-        return new GraphPathCounterPart2<>(graph.getNodes())
-                .countPaths("start", "end");
+        return graph;
     }
 }
